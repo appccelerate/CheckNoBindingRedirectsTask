50c3b29da670cdad727ff973ac3cbdca04e1e61e
Urs Enzler <urs.enzler@gmail.com>
2014-03-05 15:54:42 +0100

    added checks for binding redirects

---
+++ source/Appccelerate.CheckNoBindingRedirectsTask/CheckNoBindingRedirectsTask.cs
@@ -0,0 +1,79 @@
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="CheckNoBindingRedirectsTask.cs" company="Appccelerate">
+//   Copyright (c) 2008-2014
+//
+//   Licensed under the Apache License, Version 2.0 (the "License");
+//   you may not use this file except in compliance with the License.
+//   You may obtain a copy of the License at
+//
+//       http://www.apache.org/licenses/LICENSE-2.0
+//
+//   Unless required by applicable law or agreed to in writing, software
+//   distributed under the License is distributed on an "AS IS" BASIS,
+//   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+//   See the License for the specific language governing permissions and
+//   limitations under the License.
+// </copyright>
+// --------------------------------------------------------------------------------------------------------------------
+
+namespace Appccelerate.CheckNoBindingRedirectsTask
+{
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Xml.Linq;
+    using Microsoft.Build.Framework;
+    using Microsoft.Build.Utilities;
+
+    public class CheckNoBindingRedirectsTask : Task
+    {
+        [Required]
+        public bool TreatWarningsAsErrors { get; set; }
+
+        [Required]
+        public string ConfigFullPath { get; set; }
+
+        public string ExcludePatterns { get; set; }
+
+        public override bool Execute()
+        {
+            XDocument config = XDocument.Load(this.ConfigFullPath);
+
+            var excludePatterns =
+                this.ExcludePatterns != null
+                    ? this.ExcludePatterns.Split(',')
+                    : Enumerable.Empty<string>();
+
+            var verifier = new Verifier();
+
+            IReadOnlyCollection<Violation> violations = verifier.Verify(
+                config,
+                excludePatterns.ToList());
+
+            foreach (Violation violation in violations)
+            {
+                this.LogViolation(violation);
+            }
+
+            bool continueBuild = !(violations.Any() && this.TreatWarningsAsErrors);
+            return continueBuild;
+        }
+
+        private void LogViolation(Violation violation)
+        {
+            string message = string.Concat(
+                "Found unexpecte binding redirect for assembly `",
+                violation.Assembly,
+                "` in ",
+                this.ConfigFullPath);
+
+            if (this.TreatWarningsAsErrors)
+            {
+                this.Log.LogError(message);
+            }
+            else
+            {
+                this.Log.LogWarning(message);
+            }
+        }
+    }
+}
---
+++ source/Appccelerate.CheckNoBindingRedirectsTask/Verifier.cs
@@ -0,0 +1,44 @@
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="Verifier.cs" company="Appccelerate">
+//   Copyright (c) 2008-2014
+//
+//   Licensed under the Apache License, Version 2.0 (the "License");
+//   you may not use this file except in compliance with the License.
+//   You may obtain a copy of the License at
+//
+//       http://www.apache.org/licenses/LICENSE-2.0
+//
+//   Unless required by applicable law or agreed to in writing, software
+//   distributed under the License is distributed on an "AS IS" BASIS,
+//   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+//   See the License for the specific language governing permissions and
+//   limitations under the License.
+// </copyright>
+// --------------------------------------------------------------------------------------------------------------------
+
+namespace Appccelerate.CheckNoBindingRedirectsTask
+{
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text.RegularExpressions;
+    using System.Xml.Linq;
+
+    public class Verifier
+    {
+         public IReadOnlyCollection<Violation> Verify(XDocument config, IReadOnlyCollection<string> excludePatterns)
+         {
+             XNamespace ns = "urn:schemas-microsoft-com:asm.v1";
+             var bindingRedirects = config.Descendants(ns + "bindingRedirect").ToList();
+
+             var violations = from bindingRedirect in bindingRedirects
+                              let assemblyName = bindingRedirect.Parent.Element(ns + "assemblyIdentity").Attribute("name").Value
+                              where 
+                     !excludePatterns
+                         .Select(pattern => new Regex(pattern))
+                         .Any(regex => regex.IsMatch(assemblyName))
+                         select new Violation(assemblyName);
+
+             return violations.ToList();
+         }
+    }
+}
